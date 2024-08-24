df1de4a47d1af0eeddee3185a0c0f38b1c4aa1c3
felixpc311 <felixpuello311@gmail.com>
2024-08-23 21:02:21 -0500

    inicio del proyecto

---
+++ src/main/java/com/adso/siatv4/Siatv4Application.java
@@ -0,0 +1,13 @@
+package com.adso.siatv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Siatv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Siatv4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adso/siatv4/Siatv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adso.siatv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Siatv4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
